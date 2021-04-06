19fabbb18e13eceef331bfb5ba61f04c3ee6f01d
mel1996 <78021970+mel1996@users.noreply.github.com>
2021-04-06 21:09:21 +0300

    Add files via upload

---
+++ project2/js/main.js
@@ -0,0 +1,12 @@
+var mixer = mixitup('.container-filter');
+
+$(document).ready(function () {
+    $('a[href^="#"]').click(function () {
+        //Сохраняем значение атрибута href в переменной:
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top//можно вычесть высоту меню
+        }, 500);
+        return false;
+    });
+});
